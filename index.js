93698f7d93b1ba57e4733f0520b82d3856898aff
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-05 11:53:22 +0100

    chore: extending linting rules, self-linting

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const INDENTATION = 4;
+const MAX_COMPLEXITY = 5;
+const MAX_LINE_LENGTH = 140;
+const MAX_FUNCTION_PARAMS = 3;
+const MAX_FILE_LINES = 200;
+
 module.exports = {
-    "rules": {
-        "indent": [ "error", 4 ],
-        "complexity": [ "error", 4 ],
-        "quotes": [ "error", "single" ],
-        "array-bracket-spacing": [ "error", "always" ],
-        "object-curly-spacing": [ "error", "always" ],
-        "no-magic-numbers": [ "error", {
-            "ignore": [ 0, 1, -1 ],
-            "ignoreArrayIndexes": true,
-            "enforceConst": true
+    'parserOptions': {
+        'ecmaVersion': 6
+    },
+    'rules': {
+        'indent': [ 'error', INDENTATION ],
+        'complexity': [ 'error', MAX_COMPLEXITY ],
+        'quotes': [ 'error', 'single' ],
+        'array-bracket-spacing': [ 'error', 'always' ],
+        'object-curly-spacing': [ 'error', 'always' ],
+        'no-multiple-empty-lines': [ 'error', { 'max': 1 } ],
+        'no-magic-numbers': [ 'error', {
+            'ignore': [ 0, 1, -1 ],
+            'ignoreArrayIndexes': true,
+            'enforceConst': true
         } ],
-        "max-len": [ "error", 140 ],
-        "max-params": [ "error", 3 ],
-        "max-lines": [ "error", 200 ],
-        "no-duplicate-imports": "error",
-        "eol-last": "error"
+        'max-len': [ 'error', MAX_LINE_LENGTH ],
+        'max-params': [ 'error', MAX_FUNCTION_PARAMS ],
+        'max-lines': [ 'error', MAX_FILE_LINES ],
+        'no-duplicate-imports': 'error',
+        'eol-last': 'error',
+        'no-trailing-spaces': 'error'
     }
 };
